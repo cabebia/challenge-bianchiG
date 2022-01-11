f7c45c874c1522d66b5f09bf1459efa95d2ed806
Germando <germandbianchi@gmail.com>
2022-01-11 19:48:00 -0300

    Estructura base con conexion a DB

---
+++ src/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+
+}) .then( () => {
+    console.log("DB Connected");
+})
+
+    .catch( (er)=> {
+        console.error(er);
+    })
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express();
+
+require('./database')
+
+// Initializations
+
+// Setings
+
+// Middlewears
+
+// Global variables
+
+// Routes
+
+// Static
+
