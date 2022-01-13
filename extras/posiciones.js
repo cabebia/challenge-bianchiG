fe557ee014bd157a185891b1ce0bb265e243b696
Germando <germandbianchi@gmail.com>
2022-01-13 12:27:30 -0300

    Este era el archivo verdadero

--- extras/posiciones.js
+++ extras/posiciones.js
@@ -1,7 +1,14 @@
+require('./database');
+
 const cheerio = require('cheerio');
+
 const request = require('request');
 
+const Posiciones = require('./models/Posiciones');
+
 url = 'https://www.futbolargentino.com/primera-division/tabla-de-posiciones'
+
+const arreq = [];
 
 request(url, (err, res, body) => {
 
@@ -10,8 +17,6 @@ request(url, (err, res, body) => {
         const $ = cheerio.load(body);
 
         const equipos = $('tbody tr');
-
-        const arreq = []
 
         equipos.each((i, el) => {
 
@@ -34,8 +39,23 @@ request(url, (err, res, body) => {
 
             arreq.push(aux);
 
+
         });
 
-        console.log(arreq[0]);
+        //console.log(arreq);
+
+        try{
+            arreq.forEach(element => crear(element[0],element[1],element[2],element[3],element[4],element[5],element[6],element[7],element[8],element[9],element[10]));
+        }catch(err){
+            console.error(err);
+        }
+
+    } else{
+        console.error(err);
     }
 });
+
+const crear =  (posicion,club,imagePath,pj,pg,pe,pp,gf,gc,dg,puntos) => {
+    const test = new Posiciones({posicion:posicion,club:club,imagePath:imagePath,pj:pj,pg:pg,pe:pe,pp:pp,gf:gf,gc:gc,dg:dg,puntos:puntos});
+    const res = test.save();
+};
